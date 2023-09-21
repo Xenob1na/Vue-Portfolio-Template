ea03015592383e654ce3fa1000367eda53093c25
Gromov Danil <105980448+Xenob1na@users.noreply.github.com>
2023-09-21 16:58:17 +0700

    Alteration

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Vue-Portfolio-Template/",
   plugins: [
     vue(),
   ],
-  base: "/Portfolio-Template",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
